30edd7ca8a95cf33ee640d067985fac7e0f73495
inhumanform707 <ivica.jakelic@gmail.com>
2025-05-20 16:01:09 +0200

    Add project files.

---
+++ Ispit.Konzola/Program.cs
@@ -0,0 +1,55 @@
+﻿using Ispit.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Konzola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dobrodosli u aplikaciju za unos ucenika!");
+            Console.WriteLine("Pritisnite bilo koju tipku za nastavak...");
+            Console.ReadKey();
+            Console.Clear();
+
+            List<Ucenik> ucenici = new List<Ucenik>();
+
+            for (byte i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Unesite podatke za {i + 1}. ucenika:");
+                Console.Write("Ime: ");
+                string ime = Console.ReadLine();
+                Console.Write("Prezime: ");
+                string prezime = Console.ReadLine();
+                Console.Write("Datum rodjenja (dd.MM.yyyy): ");
+                DateTime datumRodjenja = DateTime.Parse(Console.ReadLine());
+                Console.Write("Prosjek: ");
+                double prosjek = double.Parse(Console.ReadLine());
+                Ucenik ucenik = new Ucenik(ime, prezime, datumRodjenja, prosjek);
+                ucenici.Add(ucenik);
+
+            }
+
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+            foreach (Ucenik ucenik in ucenici)
+            {
+                Console.WriteLine("\nPodaci o uceniku:");
+                ucenik.IspisDetaljaUcenika();
+                ucenik.Starost();
+                Console.Write("Prosjek: ");
+                ucenik.IspisProsjeka();
+                Console.WriteLine();
+            }
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("Aplikacija je zavrsila sa radom. Pritisnite bilo koju tipku za izlaz...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ispit.Model/Ucenik.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Model
+{
+    public class Ucenik
+    {
+        #region Properties
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public DateTime DatumRodjenja { get; set; }
+        public double Prosjek { get; set; }
+        #endregion
+
+        #region Constructors
+        public Ucenik(string ime, string prezime, DateTime datumRodjenja, double prosjek)
+        {
+            Ime = ime;
+            Prezime = prezime;
+            DatumRodjenja = datumRodjenja;
+            Prosjek = prosjek;
+        }
+        #endregion
+
+        #region Methods
+        public void Starost()
+        {
+            DateTime danas = DateTime.Now;
+            int starost = danas.Year - DatumRodjenja.Year;
+            Console.WriteLine($"Starost: {starost} godina");
+        }
+
+        public void IspisProsjeka()
+        {
+            if (Prosjek >= 1 && Prosjek <= 1.49)
+            {
+                Console.WriteLine("nedovoljan");
+            }
+            else if (Prosjek >= 1.5 && Prosjek <= 2.49)
+            {
+                Console.WriteLine("dovoljan");
+            }
+            else if (Prosjek >= 2.5 && Prosjek <= 3.49)
+            {
+                Console.WriteLine("dobar");
+            }
+            else if (Prosjek >= 3.5 && Prosjek <= 4.49)
+            {
+                Console.WriteLine("vrlo dobar");
+            }
+            else if (Prosjek >= 4.5 && Prosjek <= 5)
+            {
+                Console.WriteLine("odlican");
+            }
+        }
+
+        public void IspisDetaljaUcenika()
+        {
+            Console.WriteLine($"Ime: {Ime}, Prezime: {Prezime}, Datum rođenja: {DatumRodjenja.ToShortDateString()}, Prosjek: {Prosjek}");
+        }
+        #endregion
+    }
+}
